81b07fabcec57bc96d5bbcc8e8590c42c6689bdb
ayub <ayubtimurov3@gmail.com>
2025-11-28 11:22:23 +0500

    welcome bot is ready

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
-def main():
-    print("Hello from welcome-bot!")
+from typing import Final
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+
+TOKEN: Final = '8563593507:AAH08Fg8DBUy5tBup8MYqPhwso-aAlVbWv4'
+BOT_USERNAME: Final = '@welcomeDIIbot'
+
+# Commands
+
+async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print("Start command recieved.")
+    user = update.effective_user
+    await update.message.reply_text(f"👋 Hello {user}. Welcome to Welcome Bot😊")
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print("Help command recieved.")
+    help_text = """
+    🤖 *Bot Commands:*
+
+/start - Start the bot and get welcome message
+/help - Show this help message
+"""
+    await update.message.reply_text(help_text)
+
 
 
 if __name__ == "__main__":
-    main()
+    print("Starting...")
+    app = Application.builder().token(TOKEN).build()
+
+    app.add_handler(CommandHandler("start", start_command))
+    app.add_handler(CommandHandler("help", help_command))
+
+    print("Bot is running...")
+    app.run_polling(poll_interval=1)
